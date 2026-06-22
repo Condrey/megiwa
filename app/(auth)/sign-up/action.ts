831b7613ad93c966723b011ce1f5b57c2bc70fb6
Condrey <wangzdrey@gmail.com>
2026-06-22 14:38:41 +0300

    remove unused signUpUser function and related imports

--- app/(auth)/sign-up/action.ts
+++ app/(auth)/sign-up/action.ts
@@ -1,19 +1,6 @@
 "use server";
 
 import prisma from "@/lib/prisma";
-import { SignUpSchema } from "@/lib/validation";
-
-export async function signUpUser({ email, name, password }: SignUpSchema) {
-  return await prisma.user.upsert({
-    where: { email },
-    create: {
-      email,
-      name,
-      password,
-    },
-    update: { password, name, email },
-  });
-}
 
 export async function getUsers() {
   return await prisma.user.findMany({ select: { name: true, email: true } });
