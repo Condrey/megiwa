bf6d2e672b9ef9e345928e69c194c3324eaf12de
Condrey <wangzdrey@gmail.com>
2026-07-01 16:55:34 +0300

    introducing suspense boundaries

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,13 +1,16 @@
 import { Metadata } from "next";
+import { Suspense } from "react";
+import SignInForm from "./form-sign-in";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
-
 export default function Page() {
   return (
-    <div className="h-svh flex flex-col justify-center items-center p-4">
-      Sign in page
-    </div>
+    <main className="flex  justify-center items-center flex-col px-4 min-h-svh">
+      <Suspense>
+        <SignInForm />
+      </Suspense>
+    </main>
   );
 }
